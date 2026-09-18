66a2de2d324e22c6762a9c32f62075bf38e78d89
Vitaliy <vitalikfedytnyk@gmail.com>
2026-09-18 19:19:39 +0300

    test ci

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,5 @@ from .base import *
 
 DEBUG = False
 
+# віддає статику (адмінка, restframework, swagger ui, ...) на проді, альтернатива nginx
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
